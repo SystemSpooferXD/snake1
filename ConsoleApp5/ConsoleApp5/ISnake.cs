179f95b7f72bd226f5a8f10a29fb2850093504e7
Kettu777 <52005899+SystemSpooferXD@users.noreply.github.com>
2021-03-19 18:07:46 +0100

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/ISnake.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public interface ISnake
+    {
+        void Move();
+
+        void eatMeal();
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,76 @@
+﻿//
+//               #######################################
+//               #                                     #
+//               #       x |                           #
+//               #         |                           #
+//               #         |--------                   #
+//               #                y                    #
+//               #                                     #
+//               #######################################
+//
+//
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            bool exit = false;
+            double frameRate = 1000 / 10.0;
+            DateTime lastDate = DateTime.Now;
+            Meal meal = new Meal();
+            Snake snake = new Snake();
+            while(!exit)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo input = Console.ReadKey();
+                    
+                    switch(input.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            exit = true;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            snake.Direction = Direction.Left;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            snake.Direction = Direction.Right;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            snake.Direction = Direction.Up;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            snake.Direction = Direction.Down;
+                            break;
+
+                    }
+                }
+                if((DateTime.Now - lastDate).TotalMilliseconds >= frameRate)
+                {
+
+                    snake.Move();
+
+                    if(meal.CurrentTarget.X == snake.HeadPosition.X && meal.CurrentTarget.Y == snake.HeadPosition.Y)
+                    {
+                        snake.eatMeal();
+                        meal = new Meal();
+                        frameRate /= 1.1;
+                    }
+                    lastDate = DateTime.Now;
+                    if(snake.GameOver)
+                    {
+                        Console.Clear();
+                        exit = true;
+                        Console.WriteLine($"Koniec Gry Twój wynik to { snake.Lenght }");
+                        Console.ReadLine();
+                    }
+                }
+
+            }
+        }
+    }
+}
